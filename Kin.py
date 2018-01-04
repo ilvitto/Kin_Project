343ae0e5c65d58bfec8fe1c63708f01f0bf8fa49
naigo <lorenzo.niccolai94@gmail.com>
2018-01-04 19:27:35 +0100

    modifiche

--- Kin.py
+++ Kin.py
@@ -1,5 +1,6 @@
 from array import array
 
+import time
 import scipy
 import scipy.io as sio
 import scipy.signal as sig
@@ -14,9 +15,11 @@ from CheckNFrames import CheckNFrames
 from Descriptor import Descriptor
 from matplotlib import pyplot as plt
 import math
+from gap import gap
 
 import sklearn.cluster as cluster
 from sklearn.cluster import KMeans
+
 
 class Kin:
     def __init__(self):
@@ -66,71 +69,84 @@ class Kin:
                 #     descriptors.append(CheckNFrames()._descriptorMedian)
                 currentFrames = []
 
-        #classify the number of people
+        # classify the number of people
         classification = self.classify_people(descriptors)
-
 
         print classification.cluster_centers_
         print classification.labels_
 
-        #compute centroids of clusters
+        # compute centroids of clusters
         people = classification.cluster_centers_
 
-        #self.plot_feature(descriptors)
+        # self.plot_feature(descriptors)
         self.save_people(filename="learned_people.txt", people=people)
-
 
     def processFrames(self, frames):
         return CheckNFrames(frames)._descriptorMedian
 
     def classify_people(self, descriptors):
-        # apply k-means/hierarchical/.. etc to choose k = 1 to max_number_of_people=len(descriptors) and classify
         people = []
-        X = []
-        for i in range(len(descriptors)):
-            X.append(descriptors[i].getFeatures())
+        X = np.stack(descriptors[i].getFeatures() for i in range(len(descriptors)))
 
-        errors = []
-        classifications = []
-        for i in range(len(descriptors)):
-            classification = KMeans(n_clusters=i+1, random_state=0).fit(X)
-            # classification = cluster.MeanShift().fit(X)
-            error = self.intraClusterDistance(classification, X)
-            classifications.append(classification)
-            errors.append(error)
+        gaps, s_k, K = gap.gap_statistic(X, refs=None, B=10, K=range(1, len(descriptors) + 1), N_init=10)
+        bestKValue = gap.find_optimal_k(gaps, s_k, K)
+        print "Optimal K -> ", bestKValue
 
-        errorsPerCent = []
-        for error in errors:
-            errorsPerCent.append((max(errors) - error) / (max(errors) - min(errors)) * 100)
+        np.set_printoptions(precision=3)
+        bounding_box = np.stack(self.bounding_box(X)).transpose()
 
-        #TODO: choose the elbow GAP
-        #TODO: SUPERVISED SAVED RESULTS 
+        classification = KMeans(n_clusters=bestKValue, random_state=0).fit(X)
+
+        # TODO: SUPERVISED SAVED RESULTS
         found = False
-        threshold = 15 #DA SISTEMARE
+        threshold = 15  # DA SISTEMARE
         best = 1
-        for i in range(1,len(errorsPerCent)):
-            if errorsPerCent[i]-errorsPerCent[i-1] > threshold:
-                found = True
-            if found and errorsPerCent[i]-errorsPerCent[i-1] < threshold:
-                best = i
-                break
-        print best
-        print classifications[best-1].labels_
+        print classification.labels_
         # plt.plot(range(1, len(errors)+1), np.array(errorsPerCent))
         # plt.show()
-        return classifications[best-1]
+        return classification
 
-    #TODO: intracluster distance for all
+    # TODO: intracluster distance for all
     def intraClusterDistance(self, classification, X):
         error = 0
         max_error = 0
         for k in range(len(classification.cluster_centers_)):
             for i, x in enumerate(X):
-                if(classification.labels_[i] == k):
+                if (classification.labels_[i] == k):
                     error = np.amax([scipy.spatial.distance.euclidean(x, classification.cluster_centers_[k]), error])
             max_error = np.amax([error, max_error])
 
         return max_error
+
+    def bounding_box(self, X):
+        return X.min(0), X.max(0)
+
+    # def gap_statistic(X):
+    #     (xmin, xmax), (ymin, ymax) = bounding_box(X)
+    #     # Dispersion for real distribution
+    #     ks = range(1, 10)
+    #     Wks = zeros(len(ks))
+    #     Wkbs = zeros(len(ks))
+    #     sk = zeros(len(ks))
+    #     for indk, k in enumerate(ks):
+    #         mu, clusters = find_centers(X, k)
+    #         Wks[indk] = np.log(Wk(mu, clusters))
+    #         # Create B reference datasets
+    #         B = 10
+    #         BWkbs = zeros(B)
+    #         for i in range(B):
+    #             Xb = []
+    #             for n in range(len(X)):
+    #                 Xb.append([random.uniform(xmin, xmax),
+    #                            random.uniform(ymin, ymax)])
+    #             Xb = np.array(Xb)
+    #             mu, clusters = find_centers(Xb, k)
+    #             BWkbs[i] = np.log(Wk(mu, clusters))
+    #         Wkbs[indk] = sum(BWkbs) / B
+    #         sk[indk] = np.sqrt(sum((BWkbs - Wkbs[indk]) ** 2) / B)
+    #     sk = sk * np.sqrt(1 + 1 / B)
+    #     return (ks, Wks, Wkbs, sk)
+
 
     def plot_feature(self, descriptors):
         d1 = []
